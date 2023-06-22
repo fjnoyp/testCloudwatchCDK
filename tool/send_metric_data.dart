319c3719148950d3d12cf7bdb062a9e2ac95e808
Kyle <kylcheng@gmail.com>
2023-06-22 05:49:07 -0700

    fix2

--- tool/send_metric_data.dart
+++ tool/send_metric_data.dart
@@ -1,20 +1,30 @@
 import 'dart:io';
-void main() {
 
-  final metricName = const String.fromEnvironment('METRIC_NAME');
-  final value = const String.fromEnvironment('VALUE');
+void main() {
+  final metricName = const String.fromEnvironment('metric-name');
+  final value = const String.fromEnvironment('value');
 
   var metricNameTrimmed = metricName.trim();
   var valueTrimmed = value.trim();
   var metricNameRegex = RegExp(r'^[a-zA-Z0-9\ \_\-]+$');
   var valueRegex = RegExp(r'^[-+]?[0-9]+\.?[0-9]*$');
   if (!metricNameRegex.hasMatch(metricNameTrimmed)) {
-    print('Metric name can only contain alphanumeric characters, space character, -, and _.');
+    print(
+        'Metric name can only contain alphanumeric characters, space character, -, and _.');
     exit(1);
   }
   if (!valueRegex.hasMatch(valueTrimmed)) {
     print('Metric value must be a valid number');
     exit(1);
   }
-  Process.runSync('aws', ['cloudwatch', 'put-metric-data', '--metric-name', metricName, '--namespace', 'GithubCanaryApps', '--value', value]);
+  Process.runSync('aws', [
+    'cloudwatch',
+    'put-metric-data',
+    '--metric-name',
+    metricName,
+    '--namespace',
+    'GithubCanaryApps',
+    '--value',
+    value
+  ]);
 }
